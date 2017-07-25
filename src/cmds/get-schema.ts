7b38c98655bfac569c957e2a46b9476d384a68b2
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-07-25 12:10:50 +0200

    Add watch mode for 'get-schema' command

--- src/cmds/get-schema.ts
+++ src/cmds/get-schema.ts
@@ -1,18 +1,69 @@
 export const command = 'get-schema [endpointName]'
 export const desc = 'Download GraphQL schema'
+export const builder = {
+  watch: {
+    alias: 'w',
+    boolean: true,
+  }
+}
 
 import { existsSync } from 'fs'
 import { relative } from 'path'
+import { printSchema } from 'graphql'
 import { writeSchema } from 'graphql-config'
+import * as ora from 'ora'
 
 export async function handler(context, argv) {
-  const config = context.getConfig()
-  const endpoint = config.endpointExtension.getEndpoint(argv.endpointName)
-  console.log(`Downloading introspection from ${endpoint.url}`)
+  if (argv.watch) {
+    // FIXME: stop spinner on errors
+    const spinner = ora().start()
+    const spinnerLog = msg => spinner.text = msg
 
-  const schema = await endpoint.resolveSchema(argv.endpointName)
-  const schemaPath = relative(process.cwd(), config.schemaPath)
-  await writeSchema(config.schemaPath, schema)
-  const existed = existsSync(schemaPath)
-  console.log(`Schema file was ${existed ? 'updated' : 'created'} ${schemaPath}`)
+    while (true) {
+      const isUpdated = await update(spinnerLog)
+      if (isUpdated) {
+        spinner.stop()
+        console.log(spinner.text)
+        spinner.start()
+        spinner.text = 'Updated!'
+      } else {
+        spinner.text = 'No changes.'
+      }
+
+      spinner.text += ' Next update in 10s.'
+      await wait(10000)
+    }
+  } else {
+    return await update(console.log)
+  }
+
+  async function update(log) {
+    const config = context.getConfig()
+    const endpoint = config.endpointExtension.getEndpoint(argv.endpointName)
+
+    log(`Downloading introspection from ${endpoint.url}`)
+    const newSchema = await endpoint.resolveSchema(argv.endpointName)
+
+    try {
+      const oldSchemaSDL = config.getSchemaSDL()
+      const newSchemaSDL = printSchema(newSchema)
+      if (newSchemaSDL === oldSchemaSDL) {
+         log('No changes')
+         return false
+      }
+    } catch (_) {
+    }
+
+    const schemaPath = relative(process.cwd(), config.schemaPath)
+    await writeSchema(config.schemaPath, newSchema)
+    const existed = existsSync(schemaPath)
+    log(`Schema file was ${existed ? 'updated' : 'created'} ${schemaPath}`)
+    return true
+  }
+}
+
+function wait(interval: number): Promise<void> {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), interval);
+  })
 }
