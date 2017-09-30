5bdd31f20927a395329f8547e68056b59acf6cf7
Felix Becker <felix.b@outlook.com>
2017-09-30 14:07:09 -0700

    Support config through env vars

--- src/bin.ts
+++ src/bin.ts
@@ -10,11 +10,17 @@ installCommands()
   .strict()
   .help()
   .completion('completion', false)
+  .env('GRAPHQL')
   .usage(`Usage: ${chalk.green('graphql')} [command]`)
   .example('graphql init', 'Interactively setup .graphqlconfig file')
   .example('graphql get-schema dev', 'Update local schema to match "dev" endpoint')
   .example('graphql diff dev prod', 'Show schema diff between "dev" and "prod" endpoints')
-  .epilogue('For more information go to https://github.com/graphcool/graphql-cli')
+  .epilogue([
+    'All options can be passed as environment variables',
+    'e.g. GRAPHQL_WATCH=true is equivalent to --watch=true',
+    '',
+    'For more information go to https://github.com/graphcool/graphql-cli'
+  ].join('\n'))
   .fail((msg, err, yargs) => {
     if (err) throw err // preserve stack
     if (!showedHelp) {
