81a04515d1ae40531812015b26e54b2d6d76d832
Nilan Marktanner <nilan.marktanner@student.kit.edu>
2017-12-20 16:10:58 +0100

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import chalk from 'chalk'
 
 export const noEndpointError = new Error(
-  `You don't have any enpoint in your .graphqlconfig.
-Run ${chalk.yellow('graphql add-endpoint')} to add endpoint to your config`,
+  `You don't have any endpoint in your .graphqlconfig.
+Run ${chalk.yellow('graphql add-endpoint')} to add an endpoint to your config`,
 )
