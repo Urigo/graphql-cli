39d5ba421b451cd76dfb0c81b630d1dadecb0cd3
maticzav <matic.zavadlal@gmail.com>
2019-01-21 21:40:29 +0100

    fix: GraphQL create command
    Fixes #387

--- src/cmds/create/boilerplates.ts
+++ src/cmds/create/boilerplates.ts
@@ -1,3 +1,5 @@
+import { Template } from 'creato'
+
 export interface Boilerplate {
   name: string
   description: string
@@ -6,71 +8,104 @@ export interface Boilerplate {
 
 // a list of available boilerplate projects from:
 // https://github.com/graphql-boilerplates/
-export const defaultBoilerplates: Boilerplate[] = [
+export const defaultBoilerplates: Template[] = [
   {
     name: 'node-minimal',
     description: '"Hello World" GraphQL server',
-    repo:
-      'https://github.com/graphql-boilerplates/node-graphql-server/tree/master/minimal',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/node-graphql-server',
+      path: '/minimal',
+    },
   },
   {
     name: 'node-basic',
     description: 'Basic GraphQL server (incl. database)',
-    repo:
-      'https://github.com/graphql-boilerplates/node-graphql-server/tree/master/basic',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/node-graphql-server',
+      path: '/basic',
+    },
   },
   {
     name: 'node-advanced',
     description: 'GraphQL server (incl. database & authentication)',
-    repo:
-      'https://github.com/graphql-boilerplates/node-graphql-server/tree/master/advanced',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/node-graphql-server',
+      path: '/advanced',
+    },
   },
   {
     name: 'typescript-minimal',
     description: '"Hello World" GraphQL server',
-    repo:
-      'https://github.com/graphql-boilerplates/typescript-graphql-server/tree/master/minimal',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/typescript-graphql-server',
+      path: '/minimal',
+    },
   },
   {
     name: 'typescript-basic',
     description: 'Basic GraphQL server (incl. database)',
-    repo:
-      'https://github.com/graphql-boilerplates/typescript-graphql-server/tree/master/basic',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/typescript-graphql-server',
+      path: '/basic',
+    },
   },
   {
     name: 'typescript-advanced',
     description: 'GraphQL server (incl. database & authentication)',
-    repo:
-      'https://github.com/graphql-boilerplates/typescript-graphql-server/tree/master/advanced',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/typescript-graphql-server',
+      path: '/advanced',
+    },
   },
   {
     name: 'react-fullstack-minimal',
     description: '"Hello World" fullstack app with React & GraphQL',
-    repo:
-      'https://github.com/graphql-boilerplates/react-fullstack-graphql/tree/master/minimal',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/react-fullstack-graphql',
+      path: '/minimal',
+    },
   },
   {
     name: 'react-fullstack-basic',
     description: 'React app + GraphQL server (incl. database)',
-    repo:
-      'https://github.com/graphql-boilerplates/react-fullstack-graphql/tree/master/basic',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/react-fullstack-graphql',
+      path: '/basic',
+    },
   },
   {
     name: 'vue-fullstack-minimal',
     description: '"Hello World" fullstack app with Vue & GraphQL',
-    repo:
-      'https://github.com/graphql-boilerplates/vue-fullstack-graphql/tree/master/minimal',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/vue-fullstack-graphql',
+      path: '/minimal',
+    },
   },
   {
     name: 'vue-fullstack-basic',
     description: 'Vue app + GraphQL server (incl. database)',
-    repo:
-      'https://github.com/graphql-boilerplates/vue-fullstack-graphql/tree/master/basic',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/vue-fullstack-graphql',
+      path: '/basic',
+    },
   },
   {
     name: 'vue-fullstack-advanced',
     description: 'Vue app + GraphQL server (incl. database & authentication)',
-    repo:
-      'https://github.com/graphql-boilerplates/vue-fullstack-graphql/tree/master/advanced',
+    repo: {
+      branch: 'master',
+      uri: 'https://github.com/graphql-boilerplates/vue-fullstack-graphql',
+      path: '/advanced',
+    },
   },
 ]
--- src/cmds/create/index.ts
+++ src/cmds/create/index.ts
@@ -1,29 +1,24 @@
-import * as Zip from 'adm-zip'
+// import * as Zip from 'adm-zip'
 import chalk from 'chalk'
 import commandExists = require('command-exists')
-import * as gh from 'parse-github-url'
+import * as creato from 'creato'
+// import * as gh from 'parse-github-url'
 import { spawn } from 'cross-spawn'
 import * as fs from 'fs'
 import { padEnd } from 'lodash'
 import * as path from 'path'
-import * as request from 'request'
-import * as tmp from 'tmp'
+// import * as request from 'request'
+// import * as tmp from 'tmp'
 import * as rimraf from 'rimraf'
 
 import { Context } from '../..'
 import { defaultBoilerplates } from './boilerplates'
-import { getZipInfo } from './utils'
+// import { getZipInfo } from './utils'
 
 export const command = 'create [directory]'
 export const describe = 'Bootstrap a new GraphQL project'
 
 export const builder = {
-  boilerplate: {
-    alias: 'b',
-    describe:
-      'Full URL or repo shorthand (e.g. `owner/repo`) to boilerplate GitHub repository',
-    type: 'string',
-  },
   'no-install': {
     describe: `Don't install project dependencies`,
     type: 'boolean',
@@ -31,24 +26,23 @@ export const builder = {
   },
 }
 
-function getGitHubUrl(boilerplate: string): string | undefined {
-  const details = gh(boilerplate)
+// function getGitHubUrl(boilerplate: string): string | undefined {
+//   const details = gh(boilerplate)
 
-  if (details.host && details.owner && details.repo) {
-    const branch = details.branch ? `/tree/${details.branch}` : ''
-    return `https://${details.host}/${details.repo}${branch}`
-  }
-}
+//   if (details.host && details.owner && details.repo) {
+//     const branch = details.branch ? `/tree/${details.branch}` : ''
+//     return `https://${details.host}/${details.repo}${branch}`
+//   }
+// }
 
 export async function handler(
   context: Context,
   argv: {
-    boilerplate?: string
     directory?: string
     noInstall: boolean
   },
 ) {
-  let { boilerplate, directory, noInstall } = argv
+  let { directory, noInstall } = argv
 
   if (directory && directory.match(/[A-Z]/)) {
     console.log(
@@ -91,53 +85,45 @@ export async function handler(
     fs.mkdirSync(projectPath)
   }
 
-  // allow short handle boilerplate (e.g. `node-basic`)
-  if (boilerplate && !boilerplate.startsWith('http')) {
-    const matchedBoilerplate = defaultBoilerplates.find(
-      b => b.name === boilerplate,
-    )
-    if (matchedBoilerplate) {
-      boilerplate = matchedBoilerplate.repo
-    } else {
-      // allow shorthand GitHub URLs (e.g. `graphcool/graphcool-server-example`)
-      boilerplate = getGitHubUrl(boilerplate)
-    }
-  }
-
   // interactive selection
-  if (!boilerplate) {
-    const maxNameLength = defaultBoilerplates
-      .map(bp => bp.name.length)
-      .reduce((max, x) => Math.max(max, x), 0)
-    const choices = defaultBoilerplates.map(
-      bp => `${padEnd(bp.name, maxNameLength + 2)} ${bp.description}`,
-    )
-    const { choice } = await context.prompt<{ choice: string }>({
-      type: 'list',
-      name: 'choice',
-      message: `Choose GraphQL boilerplate project:`,
-      choices,
-    })
-
-    boilerplate = defaultBoilerplates[choices.indexOf(choice)].repo
-  }
-
-  // download repo contents
-  const zipInfo = getZipInfo(boilerplate!)
-  const downloadUrl = zipInfo.url
-  const tmpFile = tmp.fileSync()
-
-  console.log(`[graphql create] Downloading boilerplate from ${downloadUrl}...`)
-
-  await new Promise(resolve => {
-    request(downloadUrl)
-      .pipe(fs.createWriteStream(tmpFile.name))
-      .on('close', resolve)
+  const maxNameLength = defaultBoilerplates
+    .map(bp => bp.name.length)
+    .reduce((max, x) => Math.max(max, x), 0)
+  const choices = defaultBoilerplates.map(bp => {
+    return {
+      name: `${padEnd(bp.name, maxNameLength + 2)} ${bp.description}`,
+      value: bp,
+    }
+  })
+  const { choice } = await context.prompt<{ choice: creato.Template }>({
+    type: 'list',
+    name: 'choice',
+    message: `Choose GraphQL boilerplate project:`,
+    choices,
   })
 
-  const zip = new Zip(tmpFile.name)
-  zip.extractEntryTo(zipInfo.path, projectPath, false)
-  tmpFile.removeCallback()
+  const template = await creato.loadTemplate(choice, projectPath)
+
+  if (template.status !== 'ok') {
+    throw new Error(template.message)
+  }
+
+  // // download repo contents
+
+  // const downloadUrl = zipInfo.url
+  // const tmpFile = tmp.fileSync()
+
+  // console.log(`[graphql create] Downloading boilerplate from ${downloadUrl}...`)
+
+  // await new Promise(resolve => {
+  //   request(downloadUrl)
+  //     .pipe(fs.createWriteStream(tmpFile.name))
+  //     .on('close', resolve)
+  // })
+
+  // const zip = new Zip(tmpFile.name)
+  // zip.extractEntryTo(zipInfo.path, projectPath, false)
+  // tmpFile.removeCallback()
 
   // run npm/yarn install
   if (!noInstall) {
