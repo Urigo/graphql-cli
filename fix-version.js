6a06256c02f3e17468a5ab7428539554e1b11b25
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-11-23 13:15:01 -0500

    Fix dependencies as well

--- fix-version.js
+++ fix-version.js
@@ -1,6 +1,19 @@
 const path = require('path');
 const packageJsonPath = path.join(process.cwd(), './package.json');
 const packageJson = require(packageJsonPath);
-packageJson.version = packageJson.version.replace('2.2.1', '4.0.0');
+const badVersion = packageJson.version;
+const fixedVersion = badVersion.replace('2.2.1', '4.0.0');
+packageJson.version = fixedVersion;
+function fixDeps(fieldName) {
+    for (const dependency in packageJson[fieldName]) {
+        if (packageJson[fieldName][dependency] === badVersion) {
+            packageJson[fieldName][dependency] = fixedVersion;
+        }
+    }
+}
+fixDeps('dependencies');
+fixDeps('devDependencies');
+fixDeps('peerDependencies');
+fixDeps('optionalDependencies');
 const fs = require('fs');
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
