7a0358b43315e7903f4632207090065e1a9a9a66
Arda TANRIKULU <20847995+ardatan@users.noreply.github.com>
2019-11-23 13:06:14 -0500

    Add temp fix for canary versioning (#660)
    * Add temp fix for canary versioning
    
    * Oops wrong path

---
+++ fix-version.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const packageJsonPath = path.join(process.cwd(), './package.json');
+const packageJson = require(packageJsonPath);
+packageJson.version = packageJson.version.replace('2.2.1', '4.0.0');
+const fs = require('fs');
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
