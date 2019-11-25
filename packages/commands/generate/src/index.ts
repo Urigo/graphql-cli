af90919cc47fbaa07cbad3b3a2155d69f9deb4b9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-11-25 13:55:51 -0500

    Add questionm

--- packages/commands/generate/src/index.ts
+++ packages/commands/generate/src/index.ts
@@ -172,6 +172,7 @@ export const plugin: CliPlugin = {
               {
                 type: 'checkbox',
                 name: 'selections',
+                message: 'What do you want to generate?',
                 choices: [
                   {
                     value: 'backend',
