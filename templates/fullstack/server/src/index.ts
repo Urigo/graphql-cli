06bf087d674bb983ab70fa6792763cdf2fe26dab
Wojciech Trocki <wtrocki@redhat.com>
2020-03-02 21:44:15 +0000

    feat: Updated and improved sample application

--- templates/fullstack/server/src/index.ts
+++ templates/fullstack/server/src/index.ts
@@ -2,13 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import http from 'http';
 
-import { ApolloServer, PubSub } from "apollo-server-express"
-import { loadConfig } from 'graphql-config';
-import { join } from 'path';
-
-import { createKnexRuntimeContext } from '@graphback/runtime'
-import { loadResolversFiles, loadSchemaFiles } from '@graphql-toolkit/file-loading';
-import knex from 'knex'
+import { createApolloServer } from './graphql';
 
 async function start() {
   const app = express();
@@ -16,28 +10,8 @@ async function start() {
   app.use(cors());
 
   app.get('/health', (req, res) => res.sendStatus(200));
-
-  const config = await loadConfig({
-    extensions: [() => ({ name: 'generate' })]
-  });
-
-  const generateConfig = await config!.getDefault().extension('generate');
-
-  // connect to db
-  const db = knex({
-    client: generateConfig.db.database,
-    connection: generateConfig.db.dbConfig,
-  })
-
-  const pubSub = new PubSub();
-
-  const apolloServer = new ApolloServer({
-    typeDefs: loadSchemaFiles(join(__dirname, '/schema/')),
-    resolvers: loadResolversFiles(join(__dirname, '/resolvers/')),
-    context: createKnexRuntimeContext(db as any, pubSub),
-    playground: true,
-  } as any)
-
+  
+  const apolloServer = await createApolloServer();
   apolloServer.applyMiddleware({ app })
 
   const httpServer = http.createServer(app)
