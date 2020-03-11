02e83e68f9ebccc54e2e394c994be477e2bc1275
Wojciech Trocki <wtrocki@redhat.com>
2020-03-11 18:48:25 +0000

    fix: generated schema and resolvers

---
+++ templates/fullstack/server/src/resolvers/models.ts
@@ -0,0 +1,18 @@
+export const models = [
+  {
+    name: "Note",
+    pubSub: {
+      publishCreate: true,
+      publishUpdate: true,
+      publishDelete: true
+    }
+  },
+  {
+    name: "Comment",
+    pubSub: {
+      publishCreate: true,
+      publishUpdate: true,
+      publishDelete: true
+    }
+  }
+]
