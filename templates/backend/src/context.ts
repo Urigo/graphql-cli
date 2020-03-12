c9e0e50489f4b670330e18a51702ac0feb6e59a2
Wojciech Trocki <wtrocki@redhat.com>
2020-03-12 15:50:04 +0000

    chore: Remove outdated backend template

--- templates/backend/src/context.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { PubSub } from 'graphql-subscriptions'
-import knex from 'knex'
-
-export interface GraphQLContext {
-  pubsub: PubSub
-  req: express.Request
-  db: knex<any, unknown[]>
-}
