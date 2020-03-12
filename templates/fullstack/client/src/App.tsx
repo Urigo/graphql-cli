bd7961f1c09c725a6774551b4ee00dd48d7fe1a0
Wojciech Trocki <wtrocki@redhat.com>
2020-03-12 12:01:35 +0000

    fix: Refactor client side application with bootstrap styles

--- templates/fullstack/client/src/App.tsx
+++ templates/fullstack/client/src/App.tsx
@@ -1,45 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { useCreateNoteMutation, useFindAllNotesQuery } from './generated-types';
+import { useFindAllNotesQuery } from './generated-types';
+import CreateNote from './components/notes/CreateNote';
+import OneNote from './components/notes/OneNote';
 
 const App: React.FC = () => {
   const allNotes = useFindAllNotesQuery();
   allNotes.startPolling(2000);
-  const [createNote] = useCreateNoteMutation();
-  const [newNoteTitle, setNewNoteTitle] = useState();
-  const [newNoteDescription, setNewNoteDescription] = useState();
+  console.log(allNotes.data?.findAllNotes)
+
   return (
     <div>
-      <fieldset>
-        <legend>Create New Note</legend>
-        <form onSubmit={e => {
-          e.preventDefault();
-          createNote({ variables: { title: newNoteTitle, description: newNoteDescription } });
-        }}>
-          <p>
-            <label htmlFor="title">Title: </label>
-            <input
-              name="title"
-              value={newNoteTitle}
-              onChange={(e) => setNewNoteTitle(e.target.value)} />
-          </p>
-          <p>
-            <label htmlFor="description">Description: </label>
-            <input
-              name="description"
-              value={newNoteDescription}
-              onChange={(e) => setNewNoteDescription(e.target.value)} />
-          </p>
-          <input type='submit' />
-        </form>
-      </fieldset>
+      <CreateNote></CreateNote>
       <ul>
         {
-          allNotes.data && allNotes.data.findAllNotes.map((note) => (
-            <li>
-              <strong>{note.title}</strong>:&nbsp;
-              {note.description}
-            </li>
+          // TODO fix typings
+          allNotes.data && allNotes.data.findAllNotes.map((note: any) => (
+            <OneNote key={note.id} id={note.id} title={note.title} description={note.description} comments={note.comments}></OneNote>
           ))
         }
       </ul>
