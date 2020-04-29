48cca4bff69871bdf6dbbe459545beb598c2f25b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-04-29 14:16:27 +0300

    Fix commander issue

--- packages/commands/codegen/src/index.ts
+++ packages/commands/codegen/src/index.ts
@@ -1,23 +1,19 @@
 import { CliPlugin } from '@test-graphql-cli/common';
 import { CodegenExtension, CodegenContext, generate, updateContextWithCliFlags, setCommandOptions } from '@graphql-codegen/cli';
-import { Command } from 'commander';
 
 export const plugin: CliPlugin = {
   async init({ program, loadGraphQLConfig, reportError }) {
     // Let codegen manage our command instance with its own options
-    setCommandOptions(
-      program
-        .command('codegen') as Command
-    )
+    setCommandOptions(program.command('codegen') as any)
       .action(async (cliFlags: any) => {
         try {
           // Load root GraphQL Config not Project Config because Codegen will update it by looking at `project` param
-          const graphqlConfig = await loadGraphQLConfig({
+          const graphqlConfig: any = await loadGraphQLConfig({
             extensions: [CodegenExtension]
           });
           // Create Codegen Context with our loaded GraphQL Config
           const codegenContext = new CodegenContext({
-            graphqlConfig: graphqlConfig as any,
+            graphqlConfig,
           });
           // This will update Codegen Context with the options provided in CLI arguments
           updateContextWithCliFlags(codegenContext, cliFlags);
